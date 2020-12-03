76002edd3b1ed322873fa4e7d277229aaa797616
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-04 00:34:27 +0530

    background audio added

--- Assets/Scripts/GameControllers/MainMenuController.cs
+++ Assets/Scripts/GameControllers/MainMenuController.cs
@@ -2,9 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField]
+    private Button musicBtn;
+
+    [SerializeField]
+    private Sprite[] musicIcons;
+
+    private void Start()
+    {
+        CheckToPlayMusic();
+    }
+
+    void CheckToPlayMusic()
+    {
+        if(GamePreferences.GetMusicState() == 1)
+        {
+            MusicController.instance.PlayMusic(true);
+            musicBtn.image.sprite = musicIcons[1];
+        }
+        else
+        {
+            MusicController.instance.PlayMusic(false);
+            musicBtn.image.sprite = musicIcons[0];
+        }
+    }
+
     public void StartGame()
     {
         GameManager.instance.gameStartedFromMainMenu = true;
@@ -26,5 +52,19 @@ public class MainMenuController : MonoBehaviour
         Application.Quit();
     }
 
-    public void MusicButton() { }
+    public void MusicButton()
+    {
+        if(GamePreferences.GetMusicState() == 0)
+        {
+            GamePreferences.SetMusicState(1);
+            MusicController.instance.PlayMusic(true);
+            musicBtn.image.sprite = musicIcons[1];
+        }
+        else if(GamePreferences.GetMusicState() == 1)
+        {
+            GamePreferences.SetMusicState(0);
+            MusicController.instance.PlayMusic(false);
+            musicBtn.image.sprite = musicIcons[0];
+        }
+    }
 }
